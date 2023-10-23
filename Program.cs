0d88bd5f11a6c8033f91d86ca561bbcd9310c21a
fagner ferreira <fagnerrc12@gmail.com>
2023-10-23 13:23:36 -0300

    create project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+
+
+IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync("localhost");
+IPAddress ipAddress = ipHostInfo.AddressList[0];
+IPEndPoint ipEndPoint = new(IPAddress.Parse(""), 11_000);
+using Socket client = new Socket(new AddressFamily(), SocketType.Stream, ProtocolType.IPv4);
+client.Accept();
+client.Connect(ipEndPoint);
+Console.WriteLine("Hello, World!");
