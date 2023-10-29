e79cc661402a93a5013e9a52648fb371fa371ad9
fagner ferreira <fagnerrc12@gmail.com>
2023-10-29 18:57:16 -0300

    update

--- ServerClient/Client.cs
+++ ServerClient/Client.cs
@@ -63,17 +63,18 @@ namespace SD
             IPAddress localIp = IPAddress.Parse(host.AddressList.First(x => x.AddressFamily == AddressFamily.InterNetwork).ToString());
             EndPoint localEndpoint = new IPEndPoint(localIp, 1);
             using Socket client = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
 
+            client.Bind(localEndpoint);
             MulticastOption multicastOption = new(IPAddress.Parse("224.168.100.2"), localIp);
             client.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, multicastOption);
-            client.Bind(localEndpoint);
 
+            EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
             string response = "";
             while (true)
             {
                 byte[] buffer = new byte[1024];
                 int resBytes = client.ReceiveFrom(buffer, ref remoteIp);
+                Console.WriteLine(Encoding.UTF8.GetString(buffer));
                 if (resBytes == 0) break;
                 response += Encoding.UTF8.GetString(buffer, 0, resBytes);
             }
